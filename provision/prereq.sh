357ed928c2bf2244655d1d1c9003270a865e9cbb
Júlio Ballot <julio.ballot@4linux.com.br>
2021-07-12 18:17:10 -0300

    provision/prereq.sh: Adicionando nameservers alternativos

--- provision/prereq.sh
+++ provision/prereq.sh
@@ -1,3 +1,10 @@
+# Define DNS
+cat <<EOF > /etc/resolv.conf
+nameserver 8.8.8.8
+nameserver 4.2.2.2
+EOF
+
+
 if [ "$1" = "debian/buster64" ]; then
   apt update > /dev/null 2>&1  && echo "[OK] APT source list updated"
   apt install -y vim chrony gnupg2 pass> /dev/null 2>&1  && echo "[OK] vim and chrony installed"
@@ -9,3 +16,4 @@ fi
 
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config > /dev/null 2>&1  && echo "[OK] Allow SSH with Password"
 systemctl restart sshd
+
