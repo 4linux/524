e455603dae89470043b720f5a4394a961006f4b9
Júlio Ballot <julio.ballot@4linux.com.br>
2021-07-12 18:26:52 -0300

    provision/install_docker.sh: Removendo warning do apt-key add

--- provision/install_docker.sh
+++ provision/install_docker.sh
@@ -1,6 +1,7 @@
 if [ "$1" = "debian/buster64" ]; then
+  export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
   apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common > /dev/null 2>&1  && echo "[OK] Prereq for Docker"
-  curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - 
+  curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - >/dev/null 2>&1 && echo "[OK] Repository key added"
   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /dev/null 2>&1  && echo "[OK] Repository added"
   apt update > /dev/null 2>&1  && echo "[OK] APT source list updated"
   apt install -y docker-ce docker-ce-cli containerd.io docker-compose > /dev/null 2>&1  && echo "[OK] Docker installed"
