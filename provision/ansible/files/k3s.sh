7c487b7304f7f3ec436300bc71e8ebcd3b9c8b33
Odilon Sousa <me@odilon.dev>
2023-08-15 18:30:25 -0300

    Atualiza conteudo para intregação com k3s

--- provision/ansible/files/k3s.sh
+++ provision/ansible/files/k3s.sh
@@ -3,4 +3,4 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-backend none --token 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --flannel-backend none" K3S_TOKEN=12345 sh -s -
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --flannel-backend none
 curl -sfL https://get.k3s.io | K3S_TOKEN=12345 sh -s - server --flannel-backend none
-curl -sfL https://get.k3s.io | sh -s - --flannel-backend none --token 12345
+curl -sfL https://get.k3s.io | sh -s - --flannel-backend none --token 12345 --disable=traefik
